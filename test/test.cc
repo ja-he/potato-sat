d0729569ab455183cf78ddb97f6bcc2b1e0becdf
Jan Hensel <ja_he@uni-bremen.de>
2019-08-19 13:12:31 +0200

    Add another test case

--- test/test.cc
+++ test/test.cc
@@ -3,10 +3,18 @@
 #include "../src/dpll-basic.h"
 #include "catch2/catch.h"
 
-TEST_CASE("propagate removes unit clause", "[propagate_unit_clauses]")
+TEST_CASE("propagate should remove unit clause", "[propagate_unit_clauses]")
 {
   Clause_set input{ { 1 }, { 2, 3 }, { 4, 5, 6 } };
   Clause_set result{ { 2, 3 }, { 4, 5, 6 } };
+  propagate_unit_clauses(input);
+  REQUIRE(input == result);
+}
+
+TEST_CASE("propagate on empty should return empty", "[propagate_unit_clauses]")
+{
+  Clause_set input{ };
+  Clause_set result{ }; 
   propagate_unit_clauses(input);
   REQUIRE(input == result);
 }
