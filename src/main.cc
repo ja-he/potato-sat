82e36ca1da488416bbf4cb8545bb8424a83ee9e2
Jan Hensel <ja_he@uni-bremen.de>
2019-08-19 23:10:49 +0200

    Autoformat main

--- src/main.cc
+++ src/main.cc
@@ -1,21 +1,19 @@
 #include "dpll-basic.h"
+#include "parse-arguments.h"
 #include <iostream>
 #include <set>
-#include "parse-arguments.h"
-
-
 
 int
 main(int argc, char** argv)
 {
 
-  Potato_SAT_settings settings = parse(argc,argv); 
+  Potato_SAT_settings settings = parse(argc, argv);
   if (settings.parsing_error) {
     return 1;
   } else if (settings.printed_help_msg) {
-    return 0; 
+    return 0;
   }
-  print_settings(settings); 
+  print_settings(settings);
 
   /*
   Clause_set s{ { -1, 2 },     { -1, 3, 9 },  { -2, -3, 4 },
