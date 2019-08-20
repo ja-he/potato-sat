20be4b349a55c4cb3f05cb2c7394814b700eac73
Jan Hensel <ja_he@uni-bremen.de>
2019-08-20 17:25:39 +0200

    Add a test case that creates a segfault (fixed in next commit)

--- test/test.cc
+++ test/test.cc
@@ -13,8 +13,24 @@ TEST_CASE("propagate should remove unit clause", "[propagate_unit_clauses]")
 
 TEST_CASE("propagate on empty should return empty", "[propagate_unit_clauses]")
 {
-  Clause_set input{ };
-  Clause_set result{ }; 
+  Clause_set input{};
+  Clause_set result{};
   propagate_unit_clauses(input);
   REQUIRE(input == result);
+}
+
+TEST_CASE("this should not provoke a segfault", "[dpll]")
+{
+
+  Clause_set segfault_provoker = {
+    { -4, -3, -2, -1 },   { -3, -1, 2, 4 },    { -4, -2, 1, 3 },
+    { 1, 2, 3, 4 },       { -10, -9, -2, -1 }, { -9, -1, 2, 10 },
+    { -10, -2, 1, 9 },    { 1, 2, 9, 10 },     { -6, -5, -2, -1 },
+    { -5, -1, 2, 6 },     { -6, -2, 1, 5 },    { 1, 2, 5, 6 },
+    { -18, -17, -2, -1 }, { -17, -1, 2, 18 },  { -18, -2, 1, 17 },
+    { 1, 2, 17, 18 },     { -8, -7, -2, -1 },  { -7, -1, 2, 8 },
+    { -8, -2, 1, 7 },     { 1, 2, 7, 8 },      { -26, -25, -2, -1 },
+    { -25, -1, 2, 26 },   { -26, -2, 1, 25 },  { 1, 2, 25, 26 }
+  };
+  REQUIRE(dpll(segfault_provoker));
 }
